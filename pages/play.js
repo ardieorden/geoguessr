40ceb81fe0932d9b81f172a25ae393d1a145c79f
Ardie Orden <ardieorden@gmail.com>
2021-03-14 22:50:22 +0800

    Add round functionality

--- pages/play.js
+++ pages/play.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import React, { useState } from "react";
 import ReactStreetview from "../components/ReactStreetview";
-import MapGL from "react-map-gl";
+import MapGL, { Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 import mapboxgl from "mapbox-gl";
@@ -37,13 +37,11 @@ function formatNumber(num) {
 }
 
 function Play() {
-  const [lat, setLat] = useState(48.8582449);
-  const [lng, setLng] = useState(2.2923836);
-  const [guessLat, setGuessLat] = useState();
-  const [guessLng, setGuessLng] = useState();
+  const [lat, setLat] = useState(48.8618442);
+  const [lng, setLng] = useState(2.2985373);
   const [streetViewPanoramaOptions, setStreetViewPanoramaOptions] = useState({
-    position: { lat, lng },
-    pov: { heading: 92.21, pitch: 24.66 },
+    position: { lat: 48.8618442, lng: 2.2985373 },
+    pov: { heading: 253.48, pitch: -13.72 },
     zoom: 1,
     addressControl: false,
     imageDateControl: true,
@@ -53,14 +51,59 @@ function Play() {
     latitude: 30,
     longitude: 0,
     zoom: 1,
-    bearing: 0,
-    pitch: 0,
   });
   const [score, setScore] = useState(0);
   const [round, setRound] = useState(1);
 
   async function submitGuess(pointerEvent) {
     let roundScore = await calculateScore(pointerEvent);
+
+    if (round === 1) {
+      setLat(37.8063508);
+      setLng(-122.4744337);
+      setStreetViewPanoramaOptions({
+        position: { lat: 37.8044682, lng: -122.471869 },
+        pov: { heading: 285.7, pitch: -9.69 },
+        zoom: 1,
+        addressControl: false,
+        imageDateControl: true,
+        showRoadLabels: false,
+      });
+    } else if (round === 2) {
+      setLat(14.5802283);
+      setLng(120.9771238);
+      setStreetViewPanoramaOptions({
+        position: { lat: 14.5802283, lng: 120.9771238 },
+        pov: { heading: 329.54, pitch: 0.62 },
+        zoom: 1,
+        addressControl: false,
+        imageDateControl: true,
+        showRoadLabels: false,
+      });
+    } else if (round === 3) {
+      setLat(51.5030724);
+      setLng(-0.1376183);
+      setStreetViewPanoramaOptions({
+        position: { lat: 51.5030724, lng: -0.1376183 },
+        pov: { heading: 227.02, pitch: 0.01 },
+        zoom: 1,
+        addressControl: false,
+        imageDateControl: true,
+        showRoadLabels: false,
+      });
+    } else if (round === 4) {
+      setLat(35.7110421);
+      setLng(139.8039924);
+      setStreetViewPanoramaOptions({
+        position: { lat: 35.7110421, lng: 139.8039924 },
+        pov: { heading: 114.46, pitch: -0.8 },
+        zoom: 1,
+        addressControl: false,
+        imageDateControl: true,
+        showRoadLabels: false,
+      });
+    }
+
     setScore(score + Math.round(roundScore));
     setRound(round + 1);
   }
@@ -81,8 +124,6 @@ function Play() {
   }
 
   async function setGuess(pointerEvent) {
-    setGuessLat(pointerEvent.lngLat[1]);
-    setGuessLng(pointerEvent.lngLat[0]);
     return [pointerEvent.lngLat[1], pointerEvent.lngLat[0]];
   }
 
@@ -93,10 +134,36 @@ function Play() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div style={{ width: "50vw", height: "100vh", float: "left" }}>
-        <ReactStreetview
-          apiKey={process.env.NEXT_PUBLIC_MAPS_JAVASCRIPT_API_KEY}
-          streetViewPanoramaOptions={streetViewPanoramaOptions}
-        />
+        {round === 1 ? (
+          <ReactStreetview
+            apiKey={process.env.NEXT_PUBLIC_MAPS_JAVASCRIPT_API_KEY}
+            streetViewPanoramaOptions={streetViewPanoramaOptions}
+          />
+        ) : null}
+        {round === 2 ? (
+          <ReactStreetview
+            apiKey={process.env.NEXT_PUBLIC_MAPS_JAVASCRIPT_API_KEY}
+            streetViewPanoramaOptions={streetViewPanoramaOptions}
+          />
+        ) : null}
+        {round === 3 ? (
+          <ReactStreetview
+            apiKey={process.env.NEXT_PUBLIC_MAPS_JAVASCRIPT_API_KEY}
+            streetViewPanoramaOptions={streetViewPanoramaOptions}
+          />
+        ) : null}
+        {round === 4 ? (
+          <ReactStreetview
+            apiKey={process.env.NEXT_PUBLIC_MAPS_JAVASCRIPT_API_KEY}
+            streetViewPanoramaOptions={streetViewPanoramaOptions}
+          />
+        ) : null}
+        {round === 5 ? (
+          <ReactStreetview
+            apiKey={process.env.NEXT_PUBLIC_MAPS_JAVASCRIPT_API_KEY}
+            streetViewPanoramaOptions={streetViewPanoramaOptions}
+          />
+        ) : null}
       </div>
       <div style={{ width: "49vw", height: "50vh", float: "left" }}>
         <MapGL
@@ -107,14 +174,72 @@ function Play() {
           mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
           onViewportChange={setViewport}
           onClick={(pointerEvent) => submitGuess(pointerEvent)}
-        ></MapGL>
+        >
+          {round > 1 ? (
+            <Marker
+              latitude={48.8618442}
+              longitude={2.2985373}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <span>Round 1 - Eiffel Tower</span>
+            </Marker>
+          ) : null}
+          {round > 2 ? (
+            <Marker
+              latitude={37.8063508}
+              longitude={-122.4744337}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <span>Round 2 - Golden Gate Bridge</span>
+            </Marker>
+          ) : null}
+          {round > 3 ? (
+            <Marker
+              latitude={14.5802283}
+              longitude={120.9771238}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <span>Round 3 - Rizal Monument</span>
+            </Marker>
+          ) : null}
+          {round > 4 ? (
+            <Marker
+              latitude={51.5030724}
+              longitude={-0.1376183}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <span>Round 4 - Buckingham Palace</span>
+            </Marker>
+          ) : null}
+          {round > 5 ? (
+            <Marker
+              latitude={35.7110421}
+              longitude={139.8039924}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <span>Round 5 - Tokyo Skytree</span>
+            </Marker>
+          ) : null}
+        </MapGL>
       </div>
-      <div style={{ width: "49vw", height: "50vh", float: "left" }}>
-        <button onClick={() => console.log("Guess")}>Guess</button>
-        <button onClick={() => console.log("Next Round")}>Next Round</button>
-        Score: {formatNumber(score)} Round: {round}
-        <div>Maximum score per round is 50,000</div>
-      </div>
+      {round < 6 ? (
+        <div style={{ width: "49vw", height: "50vh", float: "left" }}>
+          <button onClick={() => console.log("Guess")}>Guess</button>
+          <button onClick={() => console.log("Next Round")}>Next Round</button>
+          Score: {formatNumber(score)} Round: {round}
+          <div>Maximum score per round is 50,000</div>
+        </div>
+      ) : (
+        <div>
+          That&rsquo;s all for now! <br />
+          Final Score: {formatNumber(score)}
+        </div>
+      )}
     </div>
   );
 }
